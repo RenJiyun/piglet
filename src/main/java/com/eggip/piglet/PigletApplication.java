30c6bcde95847634849d41edac5a11089d8b8e3d
renjiyun <wozaijixu@msn.cn>
2019-08-03 21:16:40 +0800

    首次提交

---
+++ src/main/java/com/eggip/piglet/PigletApplication.java
@@ -0,0 +1,13 @@
+package com.eggip.piglet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PigletApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PigletApplication.class, args);
+	}
+
+}
